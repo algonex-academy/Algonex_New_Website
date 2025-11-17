d6a0aa882044f210c55712740ceac4aec7bdb6e0
samanwaysm <samanwaysm@gmail.com>
2025-11-17 08:25:31 +0530

    finish-events6

--- algonex-backend/config/settings.py
+++ algonex-backend/config/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,13 +104,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if DATABASE_URL:
+    # Use PostgreSQL if DATABASE_URL is set (production)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Local development → SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # REST Framework settings
